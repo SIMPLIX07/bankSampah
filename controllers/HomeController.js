c05c48e169db2bd196c986de934bd1d898dfa93e
ojan <muhammadfauzan292004@gmail.com>
2026-03-06 17:45:28 +0700

    adding landing, contact, about us, settings page. and make them all to able responsiveness

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -9,3 +9,31 @@ exports.komunitas = (req, res) => {
 exports.berita = (req, res) => {
     res.render('berita');
 };
+
+exports.landing = (req, res) => {
+    res.render('landing', { activePage: 'landing' });
+};
+
+exports.aboutUs = (req, res) => {
+    res.render('aboutUs', { activePage: 'about' });
+};
+
+exports.contact = (req, res) => {
+    res.render('contact', { activePage: 'contact' });
+};
+
+exports.settings = (req, res) => {
+    res.render('settings', { activePage: 'settings' });
+};
+
+exports.settingsAccount = (req, res) => {
+    res.render('settingsAccount', { activePage: 'settings' });
+};
+
+exports.settingsProfile = (req, res) => {
+    res.render('settingsProfile', { activePage: 'settings' });
+};
+
+exports.settingsSecurity = (req, res) => {
+    res.render('settingsSecurity', { activePage: 'settings' });
+};
